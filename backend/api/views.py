641cd67a6442a92c350c154d4d1742fff77d2d1e
William Ibarra <84588576+ibarraw@users.noreply.github.com>
2023-09-05 19:45:47 -0400

    Update views.py

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,40 @@
+import os
+from django.http import JsonResponse
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
+import openai
 
 # Create your views here.
+
+openai.api_key = os.getenv('OPEN_API_KEY') # TODO: Replace .env key with your API key
+
+@csrf_exempt
+def hello_backend(request):
+    if request.method == "POST":
+        return JsonResponse({"message": "Hello from Enigma's backend"})
+    else:
+        return JsonResponse({"message": f"Invalid request method used: {request.method}"})
+
+
+
+@csrf_exempt
+def summarize_text(request):
+    if request.method == "POST":
+        # Log the raw request data
+        raw_data = request.body.decode("utf-8")
+        print("Raw request data:", raw_data)
+
+        # Parse the JSON data
+        response = openai.Completion.create(
+            model="text-davinci-003",
+            prompt=raw_data + "\n\nTL;DR",
+            temperature=0.7,
+            max_tokens=60,
+            top_p=1,
+            frequency_penalty=0,
+            presence_penalty=1
+        )
+        print("Response: ", response)
+        summary = response["choices"][0]["text"]
+        return JsonResponse({"summary": summary})
+    return JsonResponse({"error": "Invalid request method"})
