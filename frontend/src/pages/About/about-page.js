69de7ba6126c4c5c9e9e4345d04522948932b7ac
HajraRizvi7316 <rizviha@sheridancollege.ca>
2023-09-10 06:11:25 -0400

    Added About Page

---
+++ frontend/src/pages/About/about-page.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import './about-page.css';
+
+
+const About = () => {
+  return (
+    <div className="background">
+	  
+		<div className="text-wrapper">About</div>
+		{/* <img src={soundImage} alt="Description of the image" /> */}
+		
+		<div className="description">
+			<p className="about-description">Lorem ipsum dolor sit amet,
+				consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
+				labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+				exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit
+				esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+				cupidatat non proident, sunt in culpa qui officia deserunt mollit
+				anim id est laborum.</p>
+
+		</div>
+	
+
+</div>
+  );
+};
+export default About;
