9371000e2ff3771f03d0e21688b759a5a5818606
HajraRizvi7316 <rizviha@sheridancollege.ca>
2023-09-10 06:20:32 -0400

    commented it out placeholder, and added main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,177 +1,90 @@
-import './App.css';
-import React from 'react';
-import { useState, useEffect } from 'react';
-import axios from 'axios';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import "./App.css";
+import React from "react";
+import axios from "axios";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+import Button from "react-bootstrap/Button";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom" 
 
+// // Get CSRF token from cookies
+// const csrftoken = document.cookie
+//   .split(";")
+//   .find((row) => row.trim().startsWith("csrftoken="))
+//   .split("=")[1];
 
-// axios.defaults.xsrfCookieName = 'csrftoken';
-// axios.defaults.xsrfHeaderName = 'X-CSRFToken';
+// // Set up Axios to use the CSRF token
+// axios.defaults.headers.post["X-CSRFToken"] = csrftoken;
+
+// axios.defaults.xsrfCookieName = "csrftoken";
+// axios.defaults.xsrfHeaderName = "X-CSRFToken";
 // axios.defaults.withCredentials = true;
 
-// DO NOT DELETE
 const client = axios.create({
-  baseURL: "http://127.0.0.1:8000"
+  baseURL: "http://127.0.0.1:8000",
 });
 
-function App() {
 
-  const [currentUser, setCurrentUser] = useState();
-  const [registrationToggle, setRegistrationToggle] = useState(false);
-  const [email, setEmail] = useState('');
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+export default class App extends React.Component {
+  constructor (props) {
+    super(props);
 
-  useEffect(() => {
-    client.get("/api/user")
-    .then(function(res) {
-      setCurrentUser(true);
-    })
-    .catch(function(error) {
-      setCurrentUser(false);
-    });
-  }, []);
-
-  function update_form_btn() {
-    if (registrationToggle) {
-      document.getElementById("form_btn").innerHTML = "Register";
-      setRegistrationToggle(false);
-    } else {
-      document.getElementById("form_btn").innerHTML = "Log in";
-      setRegistrationToggle(true);
-    }
+    
   }
 
-  function submitRegistration(e) {
-    e.preventDefault();
-    client.post(
-      "/api/register",
-      {
-        email: email,
-        username: username,
-        password: password
-      }
-    ).then(function(res) {
-      client.post(
-        "/api/login",
-        {
-          email: email,
-          password: password
-        }
-      ).then(function(res) {
-        setCurrentUser(true);
-      });
-    });
-  }
-
-  function submitLogin(e) {
-    e.preventDefault();
-    client.post(
-      "/api/login",
-      {
-        email: email,
-        password: password
-      }
-    ).then(function(res) {
-      setCurrentUser(true);
-    });
-  }
-
-  function submitLogout(e) {
-    e.preventDefault();
-    client.post(
-      "/api/logout",
-      {withCredentials: true}
-    ).then(function(res) {
-      setCurrentUser(false);
-    });
-  }
-
-  if (currentUser) {
+  render() {
     return (
-      <div>
-        <Navbar bg="dark" variant="dark">
-          <Container>
-            <Navbar.Brand>Authentication App</Navbar.Brand>
-            <Navbar.Toggle />
-            <Navbar.Collapse className="justify-content-end">
-              <Navbar.Text>
-                <form onSubmit={e => submitLogout(e)}>
-                  <Button type="submit" variant="light">Log out</Button>
-                </form>
-              </Navbar.Text>
-            </Navbar.Collapse>
-          </Container>
-        </Navbar>
-          <div className="center">
-            <h2>You're logged in!</h2>
-          </div>
-        </div>
+      
+      <div className="bg">
+        <Router>
+          <Navbar/>
+          <Routes>
+            <Route exact path='/' element={<MainPageUpload />} />
+            <Route exact path='/about' element={<About />} />
+            {/* <Route path='/audio-summary-transcription' element={<AudioSummaryTranscription />} />
+            <Route path='/buffer-page' element={<BufferPage />} /> */}
+                
+          </Routes>
+        </Router>
+        
+        {/* <HeadingText />
+        <GenerateText />
+        <TextSummary />
+        <h1>Testing React Code</h1>
+        <TextSummarizer /> */}
+      </div>
     );
   }
-  return (
-    <div>
-    <Navbar bg="dark" variant="dark">
-      <Container>
-        <Navbar.Brand>Authentication App</Navbar.Brand>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          <Navbar.Text>
-            <Button id="form_btn" onClick={update_form_btn} variant="light">Register</Button>
-          </Navbar.Text>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    {
-      registrationToggle ? (
-        <div className="center">
-          <Form onSubmit={e => submitRegistration(e)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicUsername">
-              <Form.Label>Username</Form.Label>
-              <Form.Control type="text" placeholder="Enter username" value={username} onChange={e => setUsername(e.target.value)} />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>        
-      ) : (
-        <div className="center">
-          <Form onSubmit={e => submitLogin(e)}>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control type="email" placeholder="Enter email" value={email} onChange={e => setEmail(e.target.value)} />
-              <Form.Text className="text-muted">
-                We'll never share your email with anyone else.
-              </Form.Text>
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control type="password" placeholder="Password" value={password} onChange={e => setPassword(e.target.value)} />
-            </Form.Group>
-            <Button variant="primary" type="submit">
-              Submit
-            </Button>
-          </Form>
-        </div>
-      )
-    }
-    </div>
-  );
 }
 
-export default App;
+const appDiv = document.getElementById("app");
+render(<App />, appDiv);
+// function App() {
+//   const handleButtonClick = async () => {
+//     try {
+//       const response = await client.post("/api/hello/"); // Changed from .get to .post
+//       alert(response.data.message);
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <Navbar bg="dark" variant="dark">
+//         <Container>
+//           <Navbar.Brand>Hello from Enigma's frontend</Navbar.Brand>
+//           <Navbar.Toggle />
+//           <Navbar.Collapse className="justify-content-end">
+//             <Navbar.Text>
+//               <Button id="form_btn" variant="light" onClick={handleButtonClick}>
+//                 Click to communicate with Enigma's backend!
+//               </Button>
+//             </Navbar.Text>
+//           </Navbar.Collapse>
+//         </Container>
+//       </Navbar>
+//     </div>
+//   );
+// }
+
+// export default App;
