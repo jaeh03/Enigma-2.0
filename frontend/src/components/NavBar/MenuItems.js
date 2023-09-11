895caecb470370e20b29d447de34e9a6a2c805ad
HajraRizvi7316 <rizviha@sheridancollege.ca>
2023-09-11 11:15:16 -0400

    nav bar url added

--- frontend/src/components/NavBar/MenuItems.js
+++ frontend/src/components/NavBar/MenuItems.js
@@ -1,12 +1,12 @@
 export const MenuItems = [
   {
     title: 'Home',
-    url: '#',
+    url: '/',
     cName: 'nav-links',
   },
   {
     title: 'About',
-    url: '#',
+    url: 'about/',
     cName: 'nav-links',
   },
 
