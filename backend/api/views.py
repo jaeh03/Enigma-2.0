39e7ff28c2a66b9309a6b88555b50c23973c8e37
Chris Yeung <theneoncolorz@gmail.com>
2023-09-25 20:23:38 -0400

    Basic placeholder extended summary functions

--- backend/api/views.py
+++ backend/api/views.py
@@ -29,7 +29,7 @@ def summarize_text(request):
             model="text-davinci-003",
             prompt=raw_data + "\n\nTL;DR",
             temperature=0.7,
-            max_tokens=60,
+            max_tokens=100,
             top_p=1,
             frequency_penalty=0,
             presence_penalty=1
@@ -37,4 +37,51 @@ def summarize_text(request):
         print("Response: ", response)
         summary = response["choices"][0]["text"]
         return JsonResponse({"summary": summary})
+        # return JsonResponse({"message" : "Summarizing text"})
+    return JsonResponse({"error": "Invalid request method"})
+
+@csrf_exempt
+def summarize_text_point(request):
+    if request.method == "POST":
+        # # Log the raw request data
+        # raw_data = request.body.decode("utf-8")
+        # print("Raw request data:", raw_data)
+
+        # # Parse the JSON data
+        # response = openai.Completion.create(
+        #     model="text-davinci-003",
+        #     prompt=raw_data + "\n\nTL;DR in point form",
+        #     temperature=0.7,
+        #     max_tokens=60,
+        #     top_p=1,
+        #     frequency_penalty=0,
+        #     presence_penalty=1
+        # )
+        # print("Response: ", response)
+        # summary = response["choices"][0]["text"]
+        # return JsonResponse({"summary": summary})
+        return JsonResponse({"message" : "Summarizing text in point form"})
+    return JsonResponse({"error": "Invalid request method"})
+
+@csrf_exempt
+def summarize_text_transcript(request):
+    if request.method == "POST":
+        # # Log the raw request data
+        # raw_data = request.body.decode("utf-8")
+        # print("Raw request data:", raw_data)
+
+        # # Parse the JSON data
+        # response = openai.Completion.create(
+        #     model="text-davinci-003",
+        #     prompt=raw_data + "\n\nTL;DR in point form",
+        #     temperature=0.7,
+        #     max_tokens=60,
+        #     top_p=1,
+        #     frequency_penalty=0,
+        #     presence_penalty=1
+        # )
+        # print("Response: ", response)
+        # summary = response["choices"][0]["text"]
+        # return JsonResponse({"summary": summary})
+        return JsonResponse({"message" : "Summarizing text transcript"})
     return JsonResponse({"error": "Invalid request method"})
